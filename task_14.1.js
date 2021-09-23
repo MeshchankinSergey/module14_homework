31b215cecfb75e102c343ff32ea1daf33d1b0f80
Meshchankin Sergey <meshchankin.s@ya.ru>
2021-09-23 21:14:10 +0500

    add function 14.1

---
+++ task_14.1.js
@@ -0,0 +1,21 @@
+const person = {
+    city: "Moscow"
+   }
+
+const student = Object.create(person);
+
+student.name = "Andrei";
+student.surname = "Ivanov";
+student.age = 18;
+student.position = "developer";
+
+function printsOwnProperty (student) {
+    for (let key in student) {
+
+        if (student.hasOwnProperty(key)) {
+            console.log(key + ': ' + student[key]);
+        }   
+    }
+}
+
+printsOwnProperty (student)
